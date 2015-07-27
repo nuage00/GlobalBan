a25a3239c4bcd4dd03a9b3f2e2442e3da8e2be51
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-07-27 02:36:14 +0200

    Fixing config

--- GlobalBanConfiguration.cs
+++ GlobalBanConfiguration.cs
@@ -4,25 +4,11 @@ namespace unturned.ROCKS.GlobalBan
 {
     public class GlobalBanConfiguration : IRocketPluginConfiguration
     {
-        public string DatabaseAddress;
-        public string DatabaseUsername;
-        public string DatabasePassword;
-        public string DatabaseName;
-        public string DatabaseTableName;
-        public int DatabasePort;
-
-        public IRocketPluginConfiguration DefaultConfiguration
-        {
-            get {
-                GlobalBanConfiguration config = new GlobalBanConfiguration();
-                config.DatabaseAddress = "localhost";
-                config.DatabaseUsername = "unturned";
-                config.DatabasePassword = "password";
-                config.DatabaseName = "unturned";
-                config.DatabaseTableName = "banlist";
-                config.DatabasePort = 3306;
-                return config;
-            }
-        }
+        public string DatabaseAddress = "localhost";
+        public string DatabaseUsername = "unturned";
+        public string DatabasePassword = "password";
+        public string DatabaseName = "unturned";
+        public string DatabaseTableName = "banlist";
+        public int DatabasePort = 3306;
     }
 }
