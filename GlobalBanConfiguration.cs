9a0cbbc123c936ab270d2455c30e0fb2a825bd6c
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-07-31 03:14:34 +0200

    Fixing configuration defaults

--- GlobalBanConfiguration.cs
+++ GlobalBanConfiguration.cs
@@ -1,14 +1,25 @@
-﻿using Rocket.API;
+﻿using System;
+using Rocket.API;
 
 namespace unturned.ROCKS.GlobalBan
 {
     public class GlobalBanConfiguration : IRocketPluginConfiguration
     {
-        public string DatabaseAddress = "localhost";
-        public string DatabaseUsername = "unturned";
-        public string DatabasePassword = "password";
-        public string DatabaseName = "unturned";
-        public string DatabaseTableName = "banlist";
-        public int DatabasePort = 3306;
+        public string DatabaseAddress;
+        public string DatabaseUsername;
+        public string DatabasePassword;
+        public string DatabaseName;
+        public string DatabaseTableName;
+        public int DatabasePort;
+
+        public void LoadDefaults()
+        {
+            DatabaseAddress = "localhost";
+            DatabaseUsername = "unturned";
+            DatabasePassword = "password";
+            DatabaseName = "unturned";
+            DatabaseTableName = "banlist";
+            DatabasePort = 3306;
+        }
     }
 }
