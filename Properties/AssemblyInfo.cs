68a6ddceb001ebf401ffe4f0198553d8fc83e5bf
Vicente Pastor <persiafighter@gmail.com>
2020-02-11 10:29:51 +0000

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,9 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GlobalBan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pustalorc")]
 [assembly: AssemblyProduct("GlobalBan")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright © 2014-2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
 [assembly: Guid("34ca52ac-333c-45df-8a77-8c8f7c463faa")]
-[assembly: AssemblyVersion("1.4.0.1")]
+[assembly: AssemblyVersion("1.4.1.0")]
+[assembly: AssemblyFileVersion("1.4.1.0")]
